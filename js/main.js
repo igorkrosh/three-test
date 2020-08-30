32bcc0b669fdbc688d5c66bc962b7c78a089b9c9
Igor Kroshkin <igor.krosh@gmail.com>
2020-08-31 02:09:20 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let geometry = new THREE.BoxGeometry(10, 10, 10);
+let material = new THREE.MeshNormalMaterial();
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 25;
+
+function Render() {
+    requestAnimationFrame(Render)
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+Render();
+

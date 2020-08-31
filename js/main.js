8f02c6f9802ebce482debe68f1ae57b32621a73b
Igor Kroshkin <igor.krosh@gmail.com>
2020-08-31 16:26:26 +0300

    Шрифты + Партикли на сцене

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { GLTFLoader } from './GLTFLoader.js';
 
 let _camera, _scene, _renderer;
 let modelLoaderGLTF;
+let particle;
 
 let sceneWrapperId = 'background'
 let sceneWrapperNode;
@@ -23,6 +24,11 @@ function Init()
     _renderer = new THREE.WebGLRenderer({alpha: true});
     _renderer.setSize(sceneWrapperNode.offsetWidth, sceneWrapperNode.offsetHeight);
 
+    CreateParticle();
+    _scene.add(particle);
+
+    SetLight();
+
     LoadModels()
 
     sceneWrapperNode.appendChild(_renderer.domElement);
@@ -33,6 +39,10 @@ function Init()
 function Animate()
 {
     requestAnimationFrame(Animate);
+
+    particle.rotation.x += 0.0000;
+    particle.rotation.y -= 0.0005;
+
     _renderer.render(_scene, _camera);
 }
 
@@ -54,9 +64,13 @@ function LoadModels()
                 if (o.isMesh) o.material = new THREE.MeshNormalMaterial();
             });
 
-            model.position.y -= 7;
-            model.rotation.x = -0.16;
-            model.rotation.y = -0.7;
+            model.position.x = -0.45;
+            model.position.y = -7.35;
+            model.position.z = -3;
+
+            model.rotation.x = 0;
+            model.rotation.y = -0.66;
+            model.rotation.z = -0.09;
         }
     )
 }
@@ -67,4 +81,48 @@ function OnWindowResize()
     _camera.updateProjectionMatrix();
 
     _renderer.setSize( sceneWrapperNode.offsetWidth, sceneWrapperNode.offsetHeight );
+}
+
+function CreateParticle()
+{
+    particle = new THREE.Object3D();
+
+    let geometry = new THREE.TetrahedronGeometry(2, 0);
+
+    let material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        shading: THREE.FlatShading
+    });
+
+    for (let i = 0; i < 1000; i++) 
+    {
+        let mesh = new THREE.Mesh(geometry, material);
+
+        mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+        mesh.position.multiplyScalar(90 + (Math.random() * 700));
+        mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+
+        particle.add(mesh);
+    }
+}
+
+function SetLight()
+{
+    let ambientLight = new THREE.AmbientLight(0x999999 );
+    _scene.add(ambientLight);
+
+    let lights = [];
+
+    lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+    lights[0].position.set( 1, 0, 0 );
+
+    lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+    lights[1].position.set( 0.75, 1, 0.5 );
+
+    lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+    lights[2].position.set( -0.75, -1, 0.5 );
+
+    _scene.add( lights[0] );
+    _scene.add( lights[1] );
+    _scene.add( lights[2] );
 }
